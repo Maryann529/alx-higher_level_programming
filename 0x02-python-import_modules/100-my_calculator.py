194901295344c8f30060c3f2f800cf3050a36bc6
Maryann <maryann.pclng@gmail.com>
2022-06-02 13:11:53 +0100

    A  program that imports all functions from the file calculator_1.py and handles basic operations.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,40 +1,32 @@
 #!/usr/bin/python3
-from sys import argv
+
+from sys import argv, exit as sys_exit
+
 from calculator_1 import add, sub, mul, div
 
-if __name__ == '__main__':
-    """
-    Handles basic operations
-    Performs basic operations like addition, substraction,
-    multiplication and division between two numbers.
-    The program will execute an operation between two numbers
-    selected by the operator sent to the program.
-    """
-    l_av = len(argv) - 1
 
-    if l_av == 3:
-        operator = argv[2]
-        num_a = int(argv[1])
-        num_b = int(argv[3])
-        if operator == '+':
-            res = add(num_a, num_b)
-            print('{:d} + {:d} = {:d}'.format(num_a, num_b, res))
-            exit(0)
-        elif operator == '-':
-            res = sub(num_a, num_b)
-            print('{:d} - {:d} = {:d}'.format(num_a, num_b, res))
-            exit(0)
-        elif operator == '*':
-            res = mul(num_a, num_b)
-            print('{:d} * {:d} = {:d}'.format(num_a, num_b, res))
-            exit(0)
-        elif operator == '/':
-            res = div(num_a, num_b)
-            print('{:d} / {:d} = {:d}'.format(num_a, num_b, res))
-            exit(0)
-        else:
-            print('Unknown operator. Available operators: +, -, * and /')
-            exit(1)
-    else:
-        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
-        exit(1)
+if __name__ == "__main__":
+
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+
+    try:
+        a, op, b = argv[1:]
+
+    except ValueError:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys_exit(1)
+
+    try:
+        print(a, op, b, "=", ops[op](int(a), int(b)))
+
+    except KeyError:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys_exit(1)
+
+    except ValueError:
+        print("Invalid operand. Must be an integer")
+        sys_exit(1)
+
+    except ZeroDivisionError:
+        print("Invalid operation. Cannot divide by zero")
+        sys_exit(1)
