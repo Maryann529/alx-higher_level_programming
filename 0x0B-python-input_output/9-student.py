7ea6baa1384243495d9eace623b307620ce5007a
Maryann <maryann.pclng@gmail.com>
2022-07-05 12:52:54 +0100

    A class Student that defines a student by:

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,27 +1,15 @@
 #!/usr/bin/python3
+"""Provides a class to represent a student"""
+
+
 class Student:
+    """Definition of a student"""
     def __init__(self, first_name, last_name, age):
+        """Instantiate a student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
-        class_d = self.__dict__
-        sel_d = dict()
-
-        if type(attrs) is list:
-            for attr in attrs:
-                if type(attr) is not str:
-                    return class_d
-
-                if attr in class_d:
-                    sel_d[attr] = class_d[attr]
-
-            return sel_d
-
-        return class_d
-
-    def reload_from_json(self, json):
-        for i in json:
-            if i in self.__dict__.keys():
-                self.__dict__[i] = json[i]
+    def to_json(self):
+        """Get a JSON representation of a student"""
+        return self.__dict__
