51d5aad6c03e23d53de91397bc77a23be1c3156a
Maryann Unoarumi <maryann.pclng@gmail.com>
2022-10-18 13:12:13 +0100

    Make a GET request and print the status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,9 +2,9 @@
 // Make a GET request and print the status code
 const request = require('request');
 request(process.argv[2], (err, resp, body) => {
-	if (err) {
-		console.log(err);
-	} else if (resp) {
-		console.log(`code: ${resp.statusCode}`);
-	}
+  if (err) {
+    console.log(err);
+  } else if (resp) {
+    console.log(`code: ${resp.statusCode}`);
+  }
 });
