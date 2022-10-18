574c5dab50302471fc704c80582ee08d970e43fd
Maryann Unoarumi <maryann.pclng@gmail.com>
2022-10-18 13:26:17 +0100

    Print the title of the Star Wars movie matching the given number

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-// Print the title of the Star Wars movie matching the given number
+
 const request = require('request');
-const url = `https://swapi.co/api/films/${process.argv[2]}`;
-request(url, { json: true }, (err, resp, body) => {
-  if (err) {
-    console.log(err);
-  } else if (body) {
-    console.log(body.title);
+
+request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error(error);
   }
+  console.log(JSON.parse(body).title);
 });
