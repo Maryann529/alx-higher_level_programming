45347823ed79efdfba2dcc15dea917f2ea89c4ae
Maryann Unoarumi <maryann.pclng@gmail.com>
2022-10-18 13:10:25 +0100

    Print all characters of a Star Wars movie

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -3,18 +3,18 @@
 const request = require('request');
 const url = `https://swapi.co/api/films/${process.argv[2]}`;
 request(url, { json: true }, (err, resp, body) => {
-	if (err) {
-		console.log(err);
-	} else if (body) {
-		const characters = body.characters;
-		for (const i in characters) {
-			request(characters[i], { json: true }, (err, resp, body) => {
-				if (err) {
-					console.log(err);
-				} else if (body) {
-					console.log(body.name);
-				}
-			});
-		}
-	}
+  if (err) {
+    console.log(err);
+  } else if (body) {
+    const characters = body.characters;
+    for (const i in characters) {
+      request(characters[i], { json: true }, (err, resp, body) => {
+        if (err) {
+          console.log(err);
+        } else if (body) {
+          console.log(body.name);
+        }
+      });
+    }
+  }
 });
