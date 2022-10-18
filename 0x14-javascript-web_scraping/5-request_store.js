5ea73371a45d87a136c082f65e350e1499bbee8c
Maryann Unoarumi <maryann.pclng@gmail.com>
2022-10-18 13:14:48 +0100

    Get the contents of a webpage and store it in a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,14 +2,14 @@
 // Get the contents of a webpage and store it in a file
 const request = require('request');
 request(process.argv[2], (err, resp, body) => {
-	if (err) {
-		console.log(err);
-	} else {
-		const fs = require('fs');
-		fs.writeFile(process.argv[3], body, 'utf-8', (err) => {
-			if (err) {
-				console.log(err);
-			}
-		});
-	}
+  if (err) {
+    console.log(err);
+  } else {
+    const fs = require('fs');
+    fs.writeFile(process.argv[3], body, 'utf-8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
 });
