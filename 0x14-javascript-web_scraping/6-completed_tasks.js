e14375eabdfc31db1c45e8962ddf4820dca0bda7
Maryann Unoarumi <maryann.pclng@gmail.com>
2022-10-18 13:15:31 +0100

    Get the number of tasks completed by user id

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,19 +2,19 @@
 // Get the number of tasks completed by user id
 const request = require('request');
 request(process.argv[2], { json: true }, (err, resp, body) => {
-	if (err) {
-		console.log(err);
-	} else if (body) {
-		const tasksByUser = {};
-		for (const i in body) {
-			if (body[i].completed === true) {
-				if (tasksByUser[body[i].userId] === undefined) {
-					tasksByUser[body[i].userId] = 1;
-				} else {
-					tasksByUser[body[i].userId]++;
-				}
-			}
-		}
-		console.log(tasksByUser);
-	}
+  if (err) {
+    console.log(err);
+  } else if (body) {
+    const tasksByUser = {};
+    for (const i in body) {
+      if (body[i].completed === true) {
+        if (tasksByUser[body[i].userId] === undefined) {
+          tasksByUser[body[i].userId] = 1;
+        } else {
+          tasksByUser[body[i].userId]++;
+        }
+      }
+    }
+    console.log(tasksByUser);
+  }
 });
