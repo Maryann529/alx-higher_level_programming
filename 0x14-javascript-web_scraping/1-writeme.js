11783a963c16305edde778be42feded5da6e0593
Maryann Unoarumi <maryann.pclng@gmail.com>
2022-10-18 13:09:46 +0100

    Write a string to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 // Write a string to a file
 const fs = require('fs');
 fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
-	if (err) {
-		console.log(err);
-	}
+  if (err) {
+    console.log(err);
+  }
 });
